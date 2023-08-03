8adcb8278c154d3884b841349472bca2957a15b8
Max Koverdyaev <shadeglare@gmail.com>
2023-08-03 10:20:47 +0300

    Fixes

--- netlify/functions/hbb-stakers.js
+++ netlify/functions/hbb-stakers.js
@@ -1,8 +1,4 @@
 export const handler = async ()=> {
-  // console.log(fetch);
-  // const response = await fetch("https://api.hubbleprotocol.io/staking/hbb/users");
-  return {
-    statusCode: 200,
-    body: `Stakers here`
-  };
-};f
+  const response = await fetch("https://api.hubbleprotocol.io/staking/hbb/users");
+  return await response.json();
+};
