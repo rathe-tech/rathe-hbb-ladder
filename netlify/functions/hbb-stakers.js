6a439d76e031b02a2f6e313055205fd1bc5d954e
Max Koverdyaev <shadeglare@gmail.com>
2023-08-03 10:18:18 +0300

    Debug deploy

--- netlify/functions/hbb-stakers.js
+++ netlify/functions/hbb-stakers.js
@@ -1,5 +1,5 @@
 export const handler = async ()=> {
-  console.log(fetch);
+  // console.log(fetch);
   // const response = await fetch("https://api.hubbleprotocol.io/staking/hbb/users");
   return {
     statusCode: 200,
