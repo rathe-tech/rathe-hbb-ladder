1fc31a3a084bb041b82e592394f4f47da48aaaef
Max Koverdyaev <shadeglare@gmail.com>
2023-09-13 17:01:14 +0300

    add leaderboard method

---
+++ netlify/functions/kamino-leaderboard.js
@@ -0,0 +1,19 @@
+export const handler = async (event) => {
+  const { period } = event.queryStringParameters ?? "24h";
+  const response = await fetch(`https://api.hubbleprotocol.io/strategies/leaderboard?env=mainnet-beta&period=${period}`);
+
+  if (response.status === 200) {
+    const data = await response.text();
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "text/json"
+      },
+      body: data
+    };
+  } else {
+    return {
+      statusCode: 500
+    };
+  }
+};
