8ce26964d24795b292aa569ca1b2f5e985ee8223
Max Koverdyaev <shadeglare@gmail.com>
2023-09-13 17:03:38 +0300

    fix query string

--- netlify/functions/kamino-leaderboard.js
+++ netlify/functions/kamino-leaderboard.js
@@ -1,5 +1,5 @@
 export const handler = async (event) => {
-  const { period } = event.queryStringParameters ?? "24h";
+  const period = event.queryStringParameters?.period ?? "24h";
   const response = await fetch(`https://api.hubbleprotocol.io/strategies/leaderboard?env=mainnet-beta&period=${period}`);
 
   if (response.status === 200) {
